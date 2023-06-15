9216cb05ef7c4846b9e8739c3000df31111cdc98
MattWong-ca <matthew.yt.wong@gmail.com>
2023-06-14 23:17:43 -0400

    Create Wave.sol contract

---
+++ backend/contracts/Wave.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract Wave {
+    constructor() {
+        console.log("Hello! This is from the constructor.");
+    }
+}
